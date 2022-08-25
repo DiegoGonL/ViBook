03b0d77557bbc0bbda6690ddb5ef142a2f41548b
Lisho <lshevoweb@gmail.com>
2022-08-25 17:19:44 +0200

    Cambiado titulo en index

--- client/pages/index.js
+++ client/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to ViBook App
         </h1>
 
         <p className={styles.description}>
