791a9627fb8b7f68f25c979778aba737ff44478f
Lisho <lshevoweb@gmail.com>
2022-08-26 18:10:01 +0200

    Creadas estructura basica de paginas de la aplicación

---
+++ client/pages/parada.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Parada = () => {
+    return (
+        <div>
+           Una Parada
+        </div>
+    );
+};
+
+export default Parada;
--- client/pages/viajes.js
+++ client/pages/viajes.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import {useEffect, useState} from 'react'
+
+const componentConfig = {
+    url: process.env.NEXT_PUBLIC_URL_API+"viajes/",
+    botonNav: "Tecnicos/as",
+    titulo: "Técnicos/as EDIS",
+    tituloLista: "Listado de todos los técnicos/as registrados",
+    botonNuevo: "Nuevo Técnico/a"
+}
+
 
 const Viajes = () => {
+
+    const [elementos, setElementos] = useState([]); // Array de todas las tareas
+
+    useEffect(() => {
+        fetch(componentConfig.url)
+            .then((response) => response.json())
+            .then((data) => {
+                setElementos(data);
+                console.log(data);
+            });
+    }, []);
+
     return (
         <div>
             Viajes
