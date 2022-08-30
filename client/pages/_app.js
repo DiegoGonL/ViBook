543c1a981356515191eaa9c2756330c0e3992845
Diego <gonzalopez.diego@gmail.com>
2022-08-30 19:43:15 +0200

    Mui and joy themes unified

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,23 @@
 import '../styles/globals.css'
 import DefaultLayout from "../layouts/DefaultLayout";
+import {StyledEngineProvider} from "@mui/joy/styles";
+import { deepmerge } from '@mui/utils';
+import {    Experimental_CssVarsProvider as CssVarsProvider,
+    experimental_extendTheme as extendMuiTheme,
+} from '@mui/material/styles';
+import { extendTheme as extendJoyTheme } from '@mui/joy/styles';
+
+const muiTheme = extendMuiTheme()
+const joyTheme = extendJoyTheme()
+const theme = deepmerge(muiTheme, joyTheme)
 
 function MyApp({ Component, pageProps }) {
   return (
+          <CssVarsProvider theme={theme}>
           <DefaultLayout>
                 <Component {...pageProps} />
           </DefaultLayout>
+              </CssVarsProvider>
       )
 }
 
